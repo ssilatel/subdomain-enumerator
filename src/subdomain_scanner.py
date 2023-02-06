c3e495afbf9545abd7c51b751e6ea552987085b9
ssilatel <setaxe25@gmail.com>
2023-02-07 00:02:25 +0100

    Formatting

--- src/subdomain_scanner.py
+++ src/subdomain_scanner.py
@@ -1,15 +1,28 @@
 from collections.abc import Collection
 from concurrent.futures import ThreadPoolExecutor
+from itertools import product
 from reconlib import CRTShAPI, HackerTargetAPI, VirusTotalAPI
 
 
 class SubdomainScanner:
-    def __init__(self, domains: Collection[str], output_file: str, threads: int, virustotal_api_key: str = None):
+    def __init__(
+            self, 
+            domains: Collection[str], 
+            output_file: str, 
+            threads: int, 
+            virustotal_api_key: str = None
+        ):
         self.domains = domains
         self.output_file = output_file
         self.threads = threads
         self.observers = []
         self.virustotal_api_key = virustotal_api_key
+        self.apis = [
+            CRTShAPI(), 
+            HackerTargetAPI(), 
+            (VirusTotalAPI(api_key=self.virustotal_api_key) if self.virustotal_api_key else VirusTotalAPI())
+        ]
+        self.domain_api_products = list(product(self.domains, self.apis))
 
     def attach(self, observer):
         self.observers.append(observer)
@@ -18,14 +31,13 @@ class SubdomainScanner:
         for observer in self.observers:
             observer.update(result)
 
-    def scan_url(self, domain) -> set[str]:
-        apis = CRTShAPI(), HackerTargetAPI(), (VirusTotalAPI(api_key=self.virustotal_api_key) if self.virustotal_api_key else VirusTotalAPI())
-        return set().union(*(api.fetch_subdomains(target=domain) for api in apis))
+    def scan_url(self, product) -> set[str]:
+        return set().union(product[1].fetch_subdomains(target=product[0]))
 
     def scan(self):
         with ThreadPoolExecutor(max_workers=self.threads) as executor:
             try:
-                for result in executor.map(self.scan_url, self.domains):
+                for result in executor.map(self.scan_url, self.domain_api_products):
                     self.notify(result)
             except KeyboardInterrupt:
                 print("\n[-] Scan ended by user input")
